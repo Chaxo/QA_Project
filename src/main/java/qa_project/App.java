64e6d93a8ec698954783e6e65d606a0d58ce39f9
Chun.He <chaxo.he@gmail.com>
2022-04-26 18:25:37 +0200

    added jar and screenshot

--- src/main/java/qa_project/App.java
+++ src/main/java/qa_project/App.java
@@ -8,7 +8,7 @@ public class App {
         Customer customer = new Customer();
         MenuSystem menu = new MenuSystem();
 
-        System.out.println("Hello");
+        System.out.println(MenuSystem.welcomeMessage);
         menu.printMenu();
 
         int choice = 0;
